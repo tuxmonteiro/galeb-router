65e5c7198799c744a5cefbb84c0633c79ab13746
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-02-14 01:11:28 -0200

    EntityController: Null Pattern implemented; Backend: Health state

--- src/main/java/com/openvraas/core/controller/EntityController.java
+++ src/main/java/com/openvraas/core/controller/EntityController.java
@@ -10,6 +10,39 @@ public interface EntityController {
         CHANGE
     }
 
+    public static final EntityController NULL = new EntityController() {
+        @Override
+        public EntityController registerListenerController(
+                ListenerController listenerController) {
+            return this;
+        }
+
+        @Override
+        public void notifyListeners(JsonObject json, Action action) {
+            return;
+        }
+
+        @Override
+        public String get(String id) {
+            return "NULL";
+        }
+
+        @Override
+        public EntityController del(JsonObject json) {
+            return this;
+        }
+
+        @Override
+        public EntityController change(JsonObject json) {
+            return this;
+        }
+
+        @Override
+        public EntityController add(JsonObject json) {
+            return this;
+        }
+    };
+
     public EntityController add(JsonObject json);
 
     public EntityController del(JsonObject json);
--- src/main/java/com/openvraas/core/model/Backend.java
+++ src/main/java/com/openvraas/core/model/Backend.java
@@ -1,5 +1,25 @@
 package com.openvraas.core.model;
 
+import com.google.gson.annotations.Expose;
+
 public class Backend extends Entity {
+
+    private static final long serialVersionUID = 1L;
+
+    public enum Health {
+        HEALTHY,
+        DEADY,
+        UNKNOWN
+    }
+
+    @Expose private Health health = Health.HEALTHY;
+
+    public Health getHealth() {
+        return health;
+    }
+
+    public void setHealth(Health health) {
+        this.health = health;
+    }
 
 }
