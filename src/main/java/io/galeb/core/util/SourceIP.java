1d4340d7cb4f14b1c32800d979e2cb20d32e4442
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-04-22 10:30:53 -0300

    SourceIP: DEFAULT_SOURCE_IP defined

--- src/main/java/io/galeb/core/util/SourceIP.java
+++ src/main/java/io/galeb/core/util/SourceIP.java
@@ -2,6 +2,9 @@ package io.galeb.core.util;
 
 public interface SourceIP {
 
+    public static final String DEFAULT_SOURCE_IP = "127.0.0.1";
+
+
     //Useful http headers
     public final String HTTP_HEADER_XREAL_IP         = "X-Real-IP";
     public final String HTTP_HEADER_X_FORWARDED_FOR  = "X-Forwarded-For";
