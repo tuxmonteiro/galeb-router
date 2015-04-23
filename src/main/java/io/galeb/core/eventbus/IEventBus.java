675fbefd7d44a5b68db24e2450e8a2052ae52a6c
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-04-23 19:04:40 -0300

    New entity: Metrics & IEventBus: new methos

--- src/main/java/io/galeb/core/eventbus/IEventBus.java
+++ src/main/java/io/galeb/core/eventbus/IEventBus.java
@@ -2,10 +2,13 @@ package io.galeb.core.eventbus;
 
 import io.galeb.core.controller.EntityController.Action;
 import io.galeb.core.model.Entity;
+import io.galeb.core.model.Metrics;
 
 public interface IEventBus {
 
     public void publishEntity(Entity entity, String entityType, Action action);
+
+    public void sendMetrics(Metrics metrics) throws InterruptedException;
 
     public IEventBus setEventBusListener(EventBusListener eventBusListener);
 
---
+++ src/main/java/io/galeb/core/model/Metrics.java
@@ -0,0 +1,9 @@
+package io.galeb.core.model;
+
+public class Metrics extends Entity {
+
+    private static final long serialVersionUID = 4453537887347058918L;
+
+    public static final String METRICS_QUEUE = "METRICS_QUEUE";
+
+}
