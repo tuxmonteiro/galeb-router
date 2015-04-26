0299a89638606a2741af5907df0064fb0cd9d2b6
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-04-26 18:22:31 -0300

    io.galeb.core.util.Constants added

---
+++ src/main/java/io/galeb/core/util/Constants.java
@@ -0,0 +1,15 @@
+package io.galeb.core.util;
+
+public final class Constants {
+
+    private Constants() {
+        // static dictionary only
+    }
+
+    public static final String TRUE  = Boolean.toString(true);
+
+    public static final String FALSE = Boolean.toString(false);
+
+    public static final String PROP_ENABLE_ACCESSLOG = "io.galeb.accesslog";
+
+}
