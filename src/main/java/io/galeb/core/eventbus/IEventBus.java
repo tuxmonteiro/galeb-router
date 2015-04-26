5c8d34b25cbb3c3a054dd0758d32fcab262c7db4
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-04-26 20:46:57 -0300

    IEventBus.NULL defined

--- src/main/java/io/galeb/core/eventbus/IEventBus.java
+++ src/main/java/io/galeb/core/eventbus/IEventBus.java
@@ -6,6 +6,8 @@ import io.galeb.core.model.Metrics;
 
 public interface IEventBus {
 
+    public static final IEventBus NULL = new NullEventBus();
+
     public void publishEntity(Entity entity, String entityType, Action action);
 
     public void sendMetrics(Metrics metrics) throws InterruptedException;
