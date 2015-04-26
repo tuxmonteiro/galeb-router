130a27549a0b1a462fa38697be26239801d7a5ff
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-04-26 20:46:10 -0300

    Added NullEventBus class

---
+++ src/main/java/io/galeb/core/eventbus/NullEventBus.java
@@ -0,0 +1,32 @@
+package io.galeb.core.eventbus;
+
+import io.galeb.core.controller.EntityController.Action;
+import io.galeb.core.model.Entity;
+import io.galeb.core.model.Metrics;
+
+import javax.enterprise.inject.Alternative;
+
+@Alternative
+public class NullEventBus implements IEventBus {
+
+    @Override
+    public void publishEntity(Entity entity, String entityType, Action action) {
+        // NULL
+    }
+
+    @Override
+    public void sendMetrics(Metrics metrics) throws InterruptedException {
+        // NULL
+    }
+
+    @Override
+    public IEventBus setEventBusListener(EventBusListener eventBusListener) {
+        return this;
+    }
+
+    @Override
+    public void start() {
+        // NULL
+    }
+
+}
