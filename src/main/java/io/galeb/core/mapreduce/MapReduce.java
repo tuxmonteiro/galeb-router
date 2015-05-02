f8d84891b3f2908f365ef91d9a8a15e65a8e8b64
tuxmonteiro <marcelotmonteiro@gmail.com>
2015-05-02 15:52:27 -0300

    Interface io.galeb.core.mapreduce.MapReduce added

---
+++ src/main/java/io/galeb/core/mapreduce/MapReduce.java
@@ -0,0 +1,17 @@
+package io.galeb.core.mapreduce;
+
+import io.galeb.core.model.Metrics;
+
+import java.util.Map;
+
+public interface MapReduce {
+
+    public MapReduce setTimeOut(Long timeOut);
+
+    public Long getTimeOut();
+
+    public void addMetrics(final Metrics metrics);
+
+    public Map<String, Integer> reduce();
+
+}
