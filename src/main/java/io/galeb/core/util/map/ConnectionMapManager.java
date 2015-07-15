37941935b62d3316df0342a799bebf2305970f39
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-07-15 15:26:49 -0300

    Exposed io.galeb.cmm.ttl.* properties

--- src/main/java/io/galeb/core/util/map/ConnectionMapManager.java
+++ src/main/java/io/galeb/core/util/map/ConnectionMapManager.java
@@ -7,16 +7,22 @@ import java.util.concurrent.TimeUnit;
 
 public class ConnectionMapManager {
 
+    public static final String PROP_CMM_TTL_THREAD_ID = "io.galeb.cmm.ttl.threadId";
+    public static final String PROP_CMM_TTL_URI       = "io.galeb.cmm.ttl.uri";
+
+    static {
+        if (System.getProperty(PROP_CMM_TTL_THREAD_ID)==null) {
+            System.setProperty(PROP_CMM_TTL_THREAD_ID, "3600");
+        }
+        if (System.getProperty(PROP_CMM_TTL_URI)==null) {
+            System.setProperty(PROP_CMM_TTL_URI, "3600");
+        }
+    }
+
     public static final ConnectionMapManager INSTANCE = new ConnectionMapManager();
 
-    public static final String PROP_CMM_TTL_THREAD_ID = "io.galeb.cmm.ttl.threadId";
-    public static final String PROP_CMM_URI           = "io.galeb.cmm.ttl.uri";
-
-    private static final long TTL_THREAD_ID = Long.parseLong(System.getProperty(PROP_CMM_TTL_THREAD_ID, Long.toString(3600)));
-    private static final long TTL_URI       = Long.parseLong(System.getProperty(PROP_CMM_URI, Long.toString(3600)));
-
     private final ConcurrentHashMapExpirable<String, ConcurrentHashMapExpirable<String, Integer>> uris =
-            new ConcurrentHashMapExpirable<>(TTL_URI, TimeUnit.SECONDS, 16, 0.9f, 1);
+            new ConcurrentHashMapExpirable<>(Long.valueOf(System.getProperty(PROP_CMM_TTL_URI)), TimeUnit.SECONDS, 16, 0.9f, 1);
 
     private ConnectionMapManager() {
         // SINGLETON
@@ -27,7 +33,7 @@ public class ConnectionMapManager {
     }
 
     public ConcurrentHashMapExpirable<String, Integer> getCounterMap(String uri) {
-        uris.putIfAbsent(uri, new ConcurrentHashMapExpirable<>(TTL_THREAD_ID, TimeUnit.SECONDS, 16, 0.9f, 1));
+        uris.putIfAbsent(uri, new ConcurrentHashMapExpirable<>(Long.valueOf(System.getProperty(PROP_CMM_TTL_THREAD_ID)), TimeUnit.SECONDS, 16, 0.9f, 1));
         return uris.get(uri);
     }
 
